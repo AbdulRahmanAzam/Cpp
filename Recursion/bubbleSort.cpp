dd552cda63292f58b226172951e0f90a683fbc69
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-01-14 14:07:53 +0500

    Update bubbleSort.cpp

--- Recursion/bubbleSort.cpp
+++ Recursion/bubbleSort.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int *arr, int i, int j, int n){
-  // when j reaches end
-    if(j == n+1){
-        i++;
-        j = i+1;
+// void bubbleSort(int *arr, int i, int j, int n){
+//   // when j reaches end
+//     if(j == n+1){
+//         i++;
+//         j = i+1;
+//     }
+
+//     //when i reaches end
+//     if(i == n)
+//         return;
+//     // swap when needed
+//     if(arr[i] > arr[j])
+//         swap(arr[i], arr[j]);
+
+//   //recursive reference
+//     return bubbleSort(arr, i, j + 1, n);
+// }
+
+void sortArray(int *arr, int n){
+    if (n <= 1) return;
+    
+    for(int i = 0; i < n-1; i++){
+        if(arr[i] > arr[i+1]){
+            swap(arr[i], arr[i+1]);
+        }
     }
-
-    //when i reaches end
-    if(i == n)
-        return;
-    // swap when needed
-    if(arr[i] > arr[j])
-        swap(arr[i], arr[j]);
-
-  //recursive reference
-    return bubbleSort(arr, i, j + 1, n);
+    
+    sortArray(arr, n-1);
 }
 
 
@@ -24,7 +36,8 @@ int main(){
     int arr[] = {10,9,8,7,6,5,4,3,2,1};
     int n = 10;
     
-    bubbleSort(arr, 0, 1, n - 1);
+    // bubbleSort(arr, 0, 1, n - 1);
+    sortArray(arr,n);
     
     for(int x:arr){
         cout << x << " ";
