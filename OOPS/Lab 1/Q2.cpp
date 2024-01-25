44842130fcedd1a08b05a626bfdee15cd28d76a0
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-01-25 15:06:29 +0500

    Create Q2.cpp

---
+++ OOPS/Lab 1/Q2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int total = 0;
+    int col = 0;
+    cout << "Enter no of students" << endl;
+    cin >> col;
+    int arr[col][3]; 
+    
+    
+    for(int i = 0 ; i< col; i++){
+        cout << "\nEnter marks of the students" << endl;
+            cout << "math " << endl;
+            cin >> arr[i][0];
+            cout <<  "english " << endl;
+            cin >> arr[i][1]; 
+            cout << "science " << endl;
+            cin >> arr[i][2];
+    }
+    
+    for(int i = 0; i < col; i++){
+        total = 0;
+        
+        for(int j = 0; j< 3; j++){
+            total += arr[i][j];
+        }
+        cout << "marks " << total << endl;
+        cout << "total avg " << total / 3 << endl;
+        
+        if(avg >= 90){
+            cout << "Grade A \n\n" ;
+        }else if(avg >= 80 && total <= 89){
+            cout << "Grade B \n\n" ;
+        }else if(avg >= 70 && total <= 79){
+            cout << "Grade C \n\n" ;
+        }else if(avg >= 60 && total <= 69){
+            cout << "Grade D \n\n" ;
+        }else if(avg < 60){
+            cout << "Grade F \n\n" ;
+        }
+    }
+    
+    
+}
