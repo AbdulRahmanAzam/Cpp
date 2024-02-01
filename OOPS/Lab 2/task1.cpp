2c11a80d958cf57d957d15933bceee6942ee0d42
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-01 15:50:19 +0500

    Create task1.cpp

---
+++ OOPS/Lab 2/task1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+struct Register{
+    int courseId;
+    string courseName;
+};
+
+struct Student : Register {
+    int studentId;
+    string firstName;
+    string lastName;
+    int cellNo;
+    string email;
+};
+
+int main(){
+    Student stud[5];
+
+    for(int i = 0; i< 2; i++){
+        cout << "Enter details of " << i+1 << "student";
+        cout << "\nEnter student id" << endl;
+        cin >> stud[i].courseId ;
+        cout << "\nEnter first name" << endl;
+        cin >> stud[i].firstName;
+        cout << "\nEnter last name " << endl;
+        cin >> stud[i].lastName;
+        cout << "\nEnter cell No" << endl;
+        cin >> stud[i].cellNo;
+        cout << "\nEnter email" << endl;
+        cin >> stud[i].email ;
+    }
+
+    for(int i = 0; i< 2; i++){
+        cout << "Details of " << i+1 << "student";
+        cout << stud[i].courseId << endl;
+        cout << stud[i].firstName << endl;
+        cout << stud[i].lastName << endl;
+        cout << stud[i].cellNo << endl;
+        cout << stud[i].email  << endl;
+    }
+}
