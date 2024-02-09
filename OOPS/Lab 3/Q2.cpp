685db9957d999af9a4043956f7df6f45b52ad47c
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-09 11:10:12 +0500

    Update header Q2.cpp

--- OOPS/Lab 3/Q2.cpp
+++ OOPS/Lab 3/Q2.cpp
@@ -1,3 +1,7 @@
+// Abdul Rahman Azam
+// 23k-0061
+// date: 9/feb/2024
+
 #include <iostream>
 using namespace std;
 
@@ -5,34 +9,42 @@ class WaterBottle{
     private:
     string company;
     string color;
-    int water_capacity;
+    int water_capacityl;
+    int water_capacityml;
     
+    public:
     //getter
     string getcompany(){return this->company;}
     string getcolor(){return this->color;}
-    int getwater_capacity(){return this->water_capacity;}
+    int getwater_capacityl(){return this->water_capacityl;}
+    int getwater_capacityml(){return this->water_capacityml;}
     
     //setter
     void setcompany(string company){this->company = company;}
     void setcolor(string color){this->color = color;}
-    void setwater_capacity(int water_capacity){this->water_capacity = water_capacity;}
+    void setwater_capacity(int water_capacityml){
+        this->water_capacityml = water_capacityml;
+        this->water_capacityl = water_capacityml / 1000;
+    }
     
     //methods
-    void updatewater_capacity(int water_capacity){
-        this->water_capacity = water_capacity;
+    void updatewater_capacity(int water_capacityml){
+        this->water_capacityml = water_capacityml;
+        this->water_capacityl = water_capacityml / 1000;
     }
-}
+};
 
 int main(){
-    WaterBottle water;
+    WaterBottle bottle;
     
-    water.setcompany("Aqua");
-    water.setcolor("blue");
-    water.setwater_capacity(500);
-    
-    
-    cout << "water company " << water.getcompany() << endl << "color " << water.getcolor() << endl << "water capacity " << water.getwater_capacity() << endl;
+    bottle.setcompany("Aqua");
+    bottle.setcolor("blue");
+    bottle.setwater_capacity(500);
     
     
+    cout << "water company " << bottle.getcompany() << endl << "color " << bottle.getcolor() << endl << "water capacity in litre " << bottle.getwater_capacityl() << endl << "water capacity in millilitres" << bottle.getwater_capacityml() << endl;
     
+    bottle.updatewater_capacity(1000);
+    
+    cout << endl << "water capacity is updated to in littre " << bottle.getwater_capacityl() << endl << " in millilitre " << bottle.getwater_capacityml() << endl;
 }
