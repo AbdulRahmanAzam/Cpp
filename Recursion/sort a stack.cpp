b68e0f369b1a9b77ba61bce5a3b14066a86e72cf
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-11 20:17:18 +0500

    Create sort a stack.cpp

---
+++ Recursion/sort a stack.cpp
@@ -0,0 +1,31 @@
+// https://www.codingninjas.com/studio/problems/sort-a-stack_985275
+
+// it will put that value in a sorted way
+void sortedInsert(stack<int> &s, int num){
+	if(s.empty() || (s.top() < num)){
+		s.push(num);
+		return;
+	}
+
+	int top = s.top();
+	s.pop();
+
+	sortedInsert(s, num);
+
+	s.push(top);
+}
+
+// it will send all the values one by one from down to up
+// not from top (from down to top)
+void sortStack(stack<int> &stack)
+{
+	if(stack.empty())
+		return;
+
+	int num = stack.top();
+	stack.pop();
+
+	sortStack(stack);
+	
+	sortedInsert(stack, num);
+}
