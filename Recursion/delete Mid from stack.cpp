b23503dae30c9655cb2817ace0c5fb59bb9be3bf
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-11 15:28:12 +0500

    Create delete Mid from stack.cpp

---
+++ Recursion/delete Mid from stack.cpp
@@ -0,0 +1,26 @@
+void solve(stack<int>&s, int size, int count){
+        // base case of middle count
+        if(count == size / 2){
+            s.pop();
+            return;
+        }
+        
+        // having top element
+        int elem = s.top();
+        
+        // pop it for the recursion
+        s.pop();
+        
+        // recursive call
+        solve(s, size, count+1);
+        
+        // when base case occured push all the values one by one
+        s.push(elem);
+    }
+    
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        int count = 0;
+        solve(s, sizeOfStack, count);
+    }
