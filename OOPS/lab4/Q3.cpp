752c22d694cfc438809d8bf62b8d7ae2d86ab619
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-15 20:33:41 +0500

    Update Q3.cpp

--- OOPS/lab4/Q3.cpp
+++ OOPS/lab4/Q3.cpp
@@ -6,10 +6,10 @@ class WeekDays {
     string Days[7];
     int CurrentDay;
 
-
     public:
     //default constructor
     WeekDays(void){
+        CurrentDay = 0;
         Days[0] = "Sunday";
         Days[1] = "Monday";
         Days[2] = "Tuesday";
@@ -22,6 +22,8 @@ class WeekDays {
     WeekDays(int CurrentDay){
         if(CurrentDay > 6)
             CurrentDay %= 6;
+            
+        this->CurrentDay = CurrentDay;
 
         Days[0] = "Sunday";
         Days[1] = "Monday";
@@ -37,9 +39,14 @@ class WeekDays {
         return Days[CurrentDay];
     }
     string getNextDay(){
+        if(CurrentDay + 1 > 6)
+            return Days[0];
         return Days[CurrentDay + 1];
     }
     string getPreviousDay(){
+        if(CurrentDay - 1 < 0)
+            return Days[6];
+            
         return Days[CurrentDay - 1];
     }
     string getNthDayFromToday(int nth){
@@ -53,5 +60,15 @@ class WeekDays {
 };
 
 int main(){
-    WeekDays();
+    int n=0;
+    WeekDays W(5);
+    
+    cout << "current day " << W.getCurrentDay() << endl;
+    cout << "next day " << W.getNextDay() << endl;
+    cout << "previous day " << W.getPreviousDay() << endl;
+    
+    cout << "Enter nth number to get nth day from current day " << endl;
+    cin >> n;
+    cout << "Nth day from today is " << W.getNthDayFromToday(n);
+    
 }
