7207ed2ce898fb3c6099b60dbc012059fe781267
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-15 20:09:55 +0500

    Update Q5.cpp

--- OOPS/Lab 3/Q5.cpp
+++ OOPS/Lab 3/Q5.cpp
@@ -2,71 +2,122 @@
 // 23k-0061
 // date: 9/feb/2024
 #include <iostream>
+#include <string>
 using namespace std;
 
 class stationary_shop {
     private:
     static const int n = 100;
-    string items[n][1];
+    string items[n];
     int price[n] = {0};
-    static const int f = 100;
+
     public:
     
     void addItem(string temp, int rs){
         static int a = 0;
-        items[a][0] = temp;
+        items[a] = temp;
         price[a] = rs;
         a++;
     }
-    void removeItem(string temp){
-        for(int i=0; i< f;i++){
-            if(items[i][0] == temp){
-                items[i][0] = "";
+    void retrieveItem(string temp){
+        for(int i=0; i< n;i++){
+            if(items[i] == temp){
+                items[i] = "";
                 price[i] = 0;
             }
         }
     }
     void modifyprice(string temp, int rs){
         int i=0;
-        for(; i < f;i++){
-            if(items[i][0] == temp){
+        for(; i < n;i++){
+            if(items[i] == temp){
                 price[i] = rs;
                 break;
             }
         }
-        cout << "price of '" << items[i][0] << "' is updates succesfully" << endl << endl;
+        cout << "price of '" << items[i] << "' is updates succesfully" << endl << endl;
     }
     
     void display(){
-        for(int i = 0; i< f; i++){
+        for(int i = 0; i< n; i++){
             if(price[i] != 0){
-                cout << "Item = " << items[i][0] << "  price = " << price[i] << endl;
+                cout << "Item = " << items[i] << "  price = " << price[i] << endl;
             }
         }
         cout << endl;
+    }
+    void generatereciept(){
+        
     }
     
 };
 
 int main(){
-    stationary_shop item;
-    stationary_shop ano;
-    
-    ano.addItem("check_check",20);
-    ano.addItem("check",11);
-    
-    item.addItem("piyala", 100);
-    item.addItem("chai", 200);
-    item.addItem("4 CGPA",100);
-    
-    item.removeItem("chai");
-    
-    
-    
-    item.display();
-    cout << endl;
-    ano.display();
-    
-    item.modifyprice("piyala", 2000);
-    item.display();
+    stationary_shop s1;
+    int n = 0;
+    cout << "\nMenu:\n"
+        << "1. Add Item\n"
+        << "2. Retrieve Item List\n"
+        << "3. Modify Item Price\n"
+        << "4. Display Items with Prices\n"
+        << "5. Generate Receipt\n"
+        << "Enter your choice: ";
+        cin >> n;
+        
+    while(n != 6){
+        string temp;
+        int price;
+
+        switch(n){
+        case 1:
+        cout << "Enter name of the item " << endl;
+        cin >> temp;
+        cout << "Enter price of the item " << endl;
+        cin >> price;  
+        s1.addItem(temp, price);
+        break;
+
+        case 2:
+        cout << "Enter name of the item you want to remove " << endl;
+        cin >> temp;
+        s1.retrieveItem(temp);
+        break;
+
+        case 3:
+        cout << "Enter the name of the item you want to modify prices of " << endl;
+        cin >> temp;
+        cout << "Enter the price you want to update " << endl;
+        cin >> price;
+        s1.modifyprice(temp, price);
+        break;
+
+        case 4: 
+        s1.display();
+        break;
+
+        case 5:
+            int i = 0;
+            string done = "done";
+            string temp[100];
+            int quantity[100];
+            do{
+                cout << "Choose the item by writing its name or (done) to finish)" << endl;
+                cin >> temp[i];
+                cout << "Enter its quantity " << endl;
+                cin >> quantity[i];
+                i++;
+                
+            }while(temp[i - 1] != done);
+            break;
+        };
+
+        cout << "\nMenu:\n"
+            << "1. Add Item\n"
+            << "2. Retrieve Item List\n"
+            << "3. Modify Item Price\n"
+            << "4. Display Items with Prices\n"
+            << "5. Generate Receipt\n"
+            << "Enter your choice: ";
+            cin >> n;
+    }
 }
