2758a4bab76bd890c668d8f95fcc0018da8b721c
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-25 03:15:01 +0500

    Update Q3.cpp

--- OOPS/Assignment1/Q3.cpp
+++ OOPS/Assignment1/Q3.cpp
@@ -1,3 +1,5 @@
+//It was a good question for practice as it could be a semester project if asked to built full game
+
 #include <iostream>
 using namespace std;
 
@@ -39,169 +41,111 @@ class ChessPiece {
 
 class ChessBoard {
     private:
-    ChessPiece* board[8][8];
+    ChessPiece board[8][8];
 
     public:
     //constructor
     ChessBoard(){
-        ChessPiece p1,p2,p3,p4,p5,p6,p7,p8, 
-               r1("Rook","white",'r'),r2("Rook","white",'r'),n1("Knight","white",'n'),n2("Knight","white",'n'),b1("Bishop","white",'b'),b2("Bishop","white",'b'),q("Queen","white",'q'),k("King","white",'k'),
-               P1("Pawn","black",'p'),P2("Pawn","black",'p'),P3("Pawn","black",'p'),P4("Pawn","black",'p'),P5("Pawn","black",'p'),P6("Pawn","black",'p'),P7("Pawn","black",'p'),P8("Pawn","black",'p'), 
-               R1("Rook","black",'r'),R2("Rook","black",'r'),N1("Knight","black",'n'),N2("Knight","black",'n'),B1("Bishop","black",'b'),B2("Bishop","black",'b'),Q("Queen","black",'q'),K("King","black",'k');
 
-        board[0][0] = &R1;
-        board[0][1] = &N1;
-        board[0][2] = &B1;
-        board[0][3] = &Q;
-        board[0][4] = &K;
-        board[0][5] = &B2;
-        board[0][6] = &N2;
-        board[0][7] = &R2;
+        board[7][0] = ChessPiece("Rook", "black",'R');
+        board[7][1] = ChessPiece("Knight","black",'N');
+        board[7][2] = ChessPiece("Bishop","black",'B');
+        board[7][3] = ChessPiece("Queen","black", 'Q');
+        board[7][4] = ChessPiece("King","black",'K');
+        board[7][5] = ChessPiece("Bishop","black",'B');
+        board[7][6] = ChessPiece("Knight","black",'N');
+        board[7][7] = ChessPiece("Rook","black",'R');
 
-        board[1][0] = &p1;
-        board[1][1] = &P2;
-        board[1][2] = &P3;
-        board[1][3] = &P4;
-        board[1][4] = &P5;
-        board[1][5] = &P6;
-        board[1][6] = &P7;
-        board[1][7] = &P8;
-
+        for(int i=0; i<8; i++){
+            // for black pawns
+            board[6][i] = ChessPiece("Pawn","black",'P');
+            //for white pawns
+            board[1][i] = ChessPiece();
+        }
         for(int i=2; i<6; i++){
             for(int j=0; j<8; j++){
-                board[i][j] = nullptr;
+                board[i][j] = ChessPiece("none","none",'.');
             }
         }
 
-        board[6][0] = &p1;
-        board[6][1] = &P2;
-        board[6][2] = &P3;
-        board[6][3] = &P4;
-        board[6][4] = &P5;
-        board[6][5] = &P6;
-        board[6][6] = &P7;
-        board[6][7] = &P8;
-
-        board[7][0] = &r1;
-        board[7][1] = &n1;
-        board[7][2] = &b1;
-        board[7][3] = &k;
-        board[7][4] = &q;
-        board[7][5] = &b2;
-        board[7][6] = &n2;
-        board[7][7] = &r2;   
+        board[0][0] = ChessPiece("Rook", "white",'r');
+        board[0][1] = ChessPiece("Knight","white",'n');
+        board[0][2] = ChessPiece("Bishop","white",'b');
+        board[0][3] = ChessPiece("Queen","white", 'q');
+        board[0][4] = ChessPiece("King","white",'k');
+        board[0][5] = ChessPiece("Bishop","white",'b');
+        board[0][6] = ChessPiece("Knight","white",'n');
+        board[0][7] = ChessPiece("Rook","white",'r');
     }
 
-    void movePiece(int source, int destination){
+    bool movePiece(string source, string destination){
+        int src_c = source[0] - 'a'  , src_r = source[1] - '0'-1, 
+        dest_c = destination[0] - 'a' , dest_r = destination[1] - '0'-1;
 
-    }
-
-    void display(){
-        for(int i=0; i< 10; i++){
-            for(int j= 0; j< 10; j++){
+        // checking for placing position
+        if(board[dest_r][dest_c].getsymbol() == '.' || // if already null
+                board[dest_r][dest_c].getcolor() != board[src_r][src_c].getcolor()){ // if not my peice
                 
-                if((i == 0 || i == 9) && j == 0 && j == 9){
-                    cout << 0;
-                }else if(i == 0 || i == 9){
-                    cout << j + 'a' - 1;
-                }else if(i > 1 && i < 9 && (j == 0 || j == 9)){
-                    cout << 9 - i;
-                }else if(board[i][j] != nullptr){
-                    cout << board[i][j]->getsymbol();
+                // working for knight
+            if(board[src_r][src_c].getsymbol() == 'n' || board[src_r][src_c].getsymbol() == 'N'){
+                
+                // if move is valid
+                if((abs(src_r - dest_r) == 2) && (abs(src_c - dest_c) == 1)){
+                    return true;
                 }else {
-                    cout << '.';
+                    cout << "invalid, your move was " << board[src_r][src_c].getsymbol() << endl;
+                    return false;
                 }
-                    
-                cout << "     ";
+
+            // working for pawn
+            }else if(board[src_r][src_c].getsymbol() == 'p' || board[dest_r][dest_c].getsymbol() == 'P'){
+                
+                // for all the moves and for the first move
+                if(((dest_r - src_r == 1) && (dest_c - src_c == 0)) || ((dest_r - src_r == 2) && (dest_c - src_c == 0) && src_r == 1)) {
+                    return true;
+                // for killing move
+                }else if((abs(dest_r - src_r) == 1) && (abs(src_c - dest_c) == 1)){
+                    return true;
+
+                }else {
+                    cout << "invalid. your move was " << board[dest_r][dest_c].getsymbol()  << " dest_r " << dest_r - src_r << " dest_c " << dest_c - src_c << endl;
+                    return false;
+                }
             }
-            cout << endl << endl;
         }
+        return false;
     }
 
-};
+    void display(){    
+        cout << " " << "   ";
+        for(char ch = 'a'; ch <= 'h'; ch++) cout << ch << "   ";
+        cout << endl << endl;
+        
+        int no = 8;
+        for(int i=7; i>=0; i--){
+            cout << no << "   ";
 
+            for(int j=0; j<8; j++){
+                    cout << board[i][j].getsymbol() << "   ";
+            }
+            cout << no-- << endl << endl;
+        }
+
+        cout << " " << "   ";
+        for(char ch = 'a'; ch <= 'h'; ch++) cout << ch << "   ";
+        cout << endl;
+    }
+};
 int main(){
     ChessBoard box1;
     box1.display();
+
+    cout << (box1.movePiece("g1","f3") ? "valid" : "invalid") << endl;
+    box1.display();
+    
+    cout << (box1.movePiece("b1","c3") ? "valid" : "invalid") << endl;
+    box1.display();
+
+    cout << (box1.movePiece("a2","a4") ? "valid" : "invalid") << endl;
+    box1.display();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for(int i=0; i<10;i++){
-        //     for(int j=0; j < 10; j++){
-        //         // to print numbers 
-        //         if((j == 0 || j == 9 ) && (i > 0 && i < 9)){
-        //             board[i][j] = 9 - i + '0';
-
-        //         }else if(i == 0 || i == 9){
-        //             if(j > 0 && j < 9)
-        //                 board[i][j] = j + 'a' - 1;
-        //             else 
-        //                 board[i][j] = ' ';
-
-        //         }else if(i == 1){
-        //             switch(j){
-        //                 case 1:
-        //                 case 8:
-        //                     board[i][j] = 'R';
-        //                     break;
-        //                 case 2:
-        //                 case 7:
-        //                     board[i][j] = 'N';
-        //                     break;
-        //                 case 3:
-        //                 case 6:
-        //                     board[i][j] = 'B';
-        //                     break;
-        //                 case 4:
-        //                     board[i][j] = 'Q';
-        //                     break;
-        //                 case 5:
-        //                     board[i][j] = 'K';
-        //                     break; 
-        //             }
-        //         }else if(i == 2){
-        //             board[i][j] = 'P';
-        //         }else if(i == 7){
-        //             board[i][j] = 'p';
-        //         }else if(i > 2 && i < 7){
-        //             board[i][j] = '.';
-        //         }else if(i == 8){
-        //             switch(j){
-        //                 case 1:
-        //                 case 8:
-        //                     board[i][j] = 'R' + 32;
-        //                     break;
-        //                 case 2:
-        //                 case 7:
-        //                     board[i][j] = 'N' + 32;
-        //                     break;
-        //                 case 3:
-        //                 case 6:
-        //                     board[i][j] = 'B' + 32;
-        //                     break;
-        //                 case 5:
-        //                     board[i][j] = 'Q' + 32;
-        //                     break;
-        //                 case 4:
-        //                     board[i][j] = 'K' + 32;
-        //                     break; 
-        //             }
-        //         }
-        //     }
-        // }
