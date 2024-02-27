36e66173930676f363d38cbef835f5bc721589d2
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-02-27 12:54:23 +0500

    Update Q1.cpp

--- OOPS/Assignment1/Q1.cpp
+++ OOPS/Assignment1/Q1.cpp
@@ -1,99 +1,200 @@
 // Abdul Rahman Azam
 // 23k-0061
-
+// took little bit help form geek for geek
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Pet {
-    private:
+private:
+    string name;
     string healthStatus;
     int hungerLevel;
     int happinessLevel;
-    string specialSkill[5];
+    string specialSkills;
 
-    public:
-    Pet(){
+public:
+    // Default constructor
+    Pet() {
         healthStatus = "Healthy";
         hungerLevel = 3;
         happinessLevel = 3;
-        specialSkill[0] = "none";
     }
 
-    void displayPetDetails(){
-        cout << "health Status = " << this->healthStatus 
-        << "\nHappiness Level = " << this->happinessLevel
-        << "\nHunger Level = " << this->hungerLevel
-        << "\nspecial Skills " << endl;
+    // Parameterized constructor
+    Pet(string name, string healthStatus, int hungerLevel, int happinessLevel,string specialSkills) {
+        this->name = name;
+        this->healthStatus = healthStatus;
+        this->happinessLevel = happinessLevel;
+        this->hungerLevel = hungerLevel;
+        this->specialSkills = specialSkills;
+    }
 
-        for(int i=0; i< specialSkill->length(); i++){
-            cout << "\t" << specialSkill[i] << endl;
+    void displayPetDetails() {
+        cout << "Name: " << name << endl;
+        cout << "Health Status: " << healthStatus << endl;
+        cout << "Happiness Level: " << happinessLevel << endl;
+        cout << "Hunger Level: " << hungerLevel << endl;
+        cout << "Special Skills:" << specialSkills;
+    }
+
+    void updateHappiness() {
+        if (hungerLevel <= 5) {
+            happinessLevel--;
+        } else if (hungerLevel >= 10) {
+            cout << "Happiness level is at maximum." << endl;
+        } else {
+            happinessLevel++;
         }
     }
 
-    void updateHappiness(){
-        if(hungerLevel <= 5){
-            this->happinessLevel--;
-        }else if(hungerLevel == 10){
-            cout << "Happiness level is 100% max" << endl;
-        }else
-            this->happinessLevel++;
-    }
-
-    void updateHealth(string healthStatus){
+    void updateHealth(string healthStatus) {
         this->healthStatus = healthStatus;
     }
-    
-    void updateHunger(int hunger){
-        this->hungerLevel = hunger;
+
+    void updateHunger(int hunger) {
+        hungerLevel = hunger;
     }
 };
 
-class Adopter{
-    private:
+class Adopter {
+private:
     string adopterName;
     int adopterMobileNum;
     vector<Pet*> adoptedPetRecords;
 
-    public:
-    //constructor
-    Adopter(string adopterName, int adopterMobileNum){
+public:
+    Adopter(string adopterName, int adopterMobileNum) {
         this->adopterName = adopterName;
         this->adopterMobileNum = adopterMobileNum;
     }
 
-    void adoptPet(Pet& obj){
-        adoptedPetRecords.push_back(&obj);
+    void adoptPet(Pet* obj) {
+        cout << adopterName << " with mobile number " << adopterMobileNum << " is adopting a pet." << endl;
+        adoptedPetRecords.push_back(obj);
     }
 
-    void returnPet(){
-        adoptedPetRecords.pop_back();
+    void returnPet() {
+        if (!adoptedPetRecords.empty()) {
+            cout << adopterName << " is returning the adopted pet." << endl;
+            adoptedPetRecords.pop_back();
+        } else {
+            cout << "No pet to return." << endl;
+        }
     }
 
-    void displayAdoptedPets(){
-        for(Pet* petinfo : adoptedPetRecords){
-            petinfo->displayPetDetails();
-            cout << endl;
+    void displayAdoptedPets() {
+        if (adoptedPetRecords.empty()) {
+            cout << adopterName << " has not adopted any pets yet." << endl;
+        } else {
+            cout << adopterName << "'s adopted pets:" << endl;
+            for (Pet* pet : adoptedPetRecords) {
+                pet->displayPetDetails();
+                cout << endl;
+            }
         }
     }
 };
 
-int main(){
-    Pet cat, dog, monkey;
-    Adopter a1("Abdul Rahman Azam", 03163677527);
+int main() {
+    vector<Pet> pets;
+    vector<Adopter> adopters;
 
-    
-    a1.adoptPet(cat);
-    a1.adoptPet(dog);
-    
+    while (true) {
+        cout << "\nVirtual Pet Adoption System\n"
+             << "1. Add Pet\n"
+             << "2. Display all Pets\n"
+             << "3. Adopt a Pet\n"
+             << "4. Return a Pet\n"
+             << "5. Display all adopted Pets\n"
+             << "6. Exit\n"
+             << "Enter your choice: ";
+        int choice;
+        cin >> choice;
 
-    cat.updateHunger(6);
-    cat.updateHealth("Healthy");
-    cat.updateHappiness();
-    
-    dog.updateHunger(2);
-    dog.updateHealth("Sick");
-    dog.updateHappiness();
+        switch (choice) {
+            case 1: {
+                string name, healthStatus;
+                int hungerLevel, happinessLevel;
+                string specialSkills;
 
-    a1.displayAdoptedPets();
+                cout << "Enter pet details:" << endl;
+                cout << "Name: ";
+                cin >> name;
+                cout << "Health Status: ";
+                cin >> healthStatus;
+                cout << "Hunger Level: ";
+                cin >> hungerLevel;
+                cout << "Happiness Level: ";
+                cin >> happinessLevel;
+
+                int numSkills;
+                cout << "Enter special skills:" << endl;
+                    cin >> specialSkills;
+        
+
+                Pet newPet(name, healthStatus, hungerLevel, happinessLevel, specialSkills);
+                pets.push_back(newPet);
+                break;
+            }
+            case 2: {
+                cout << "\nAll Pets:" << endl;
+                for (Pet& pet : pets) {
+                    pet.displayPetDetails();
+                    cout << endl;
+                }
+                break;
+            }
+            case 3: {
+                if (!pets.empty()) {
+                    cout << "Enter adopter details:" << endl;
+                    string name;
+                    int mobileNum;
+                    cout << "Name: ";
+                    cin >> name;
+                    cout << "Mobile Number: ";
+                    cin >> mobileNum;
+                    Adopter newAdopter(name, mobileNum);
+
+                    cout << "Enter index of pet to adopt: ";
+                    int index;
+                    cin >> index;
+                    if (index >= 0 && index < pets.size()) {
+                        newAdopter.adoptPet(&pets[index]);
+                        adopters.push_back(newAdopter);
+                    } else {
+                        cout << "Invalid index." << endl;
+                    }
+                } else {
+                    cout << "No pets available for adoption." << endl;
+                }
+                break;
+            }
+            case 4: {
+                if (!adopters.empty()) {
+                    int index = adopters.size() - 1;
+                    adopters[index].returnPet();
+                } else {
+                    cout << "No adopters." << endl;
+                }
+                break;
+            }
+            case 5: {
+                for (Adopter& adopter : adopters) {
+                    adopter.displayAdoptedPets();
+                }
+                break;
+            }
+            case 6: {
+                cout << "Exiting program." << endl;
+                return 0;
+            }
+            default: {
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
