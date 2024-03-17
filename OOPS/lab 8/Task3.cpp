5279dad184a630c6bc1b92e540f66eb1635cad6f
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-03-17 08:44:55 +0500

    Create Task3.cpp

---
+++ OOPS/lab 8/Task3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+class Position{
+    public:
+    int x,
+    y,
+    z;
+    
+    Position(int x, int y, int z) : x(x), y(y), z(z) {}
+    
+    void display(){
+        cout << "Position = " << x << " " << y << " " << z << endl;
+    } 
+    
+};
+
+class Health{
+    public:
+    int health;
+    
+    Health(int health) : health(health) {}
+    void display(){
+        cout << "health = " << health << endl;
+    }
+};
+
+class Character : public Position, public Health{
+    public:
+    string name;
+    int age;
+    int power;
+    
+    Character(string name, int age, int power, int health, int x, int y, int z) : name(name), age(age), power(power), Health(health), Position(x,y,z) {}
+    
+    void display(){
+        Position::display();
+        Health::display();
+        cout << "name = " << name
+        << "\nage = " << age
+        << "\npower = " << power << endl;
+    }
+};
+
+int main(){
+    Character c1("Abdul Rahman Azam", 19, 100, 60, 2,4,8);
+    c1.display();
+}
