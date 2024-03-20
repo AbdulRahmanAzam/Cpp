49059b94eb2aec110186591c5842a45e8d725632
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-03-21 03:26:37 +0500

    Update NQ2.cpp

--- OOPS/Lab Mid B 2024/NQ2.cpp
+++ OOPS/Lab Mid B 2024/NQ2.cpp
@@ -26,52 +26,104 @@ class Movie{
 class Playlist{
     public:
     string name;
-    Movie* movies;
-    int NumMovies;
+    Movie** movies;
+    int NumMovies = 0;
     
-    Playlist(const string& name) : name(name) {}
+    Playlist(const string& name) : name(name), movies(nullptr) {
+        // movies = new Movie*[200];
+    }
     
     void addMovie(Movie* movie){
-        Movie newmovie = new Movie*[4];
-        movies[NumMovies] = *movie;
+        Movie** temp = new Movie*[NumMovies + 1];
+        
+        for(int i=0; i< NumMovies ; i++){
+            temp[i] = movies[i];
+        }
+        temp[NumMovies] = movie;
+        delete[] movies;
+        movies = temp;
         NumMovies++;
     }
     void removeMovie(Movie* movie){
-        movies[NumMovies];
+        
+        for(int i=0; i<NumMovies - 1; i++){
+            movies[i] = movies[i+1];
+        }
         NumMovies--;
     }
     void displayInfo() const{
         cout << "movies" << endl;
         for(int i=0; i< NumMovies ; i++){
-            cout << "movie name = " << movies[i].title << endl;
+            cout << "movie name = " << movies[i]->title << endl;
         }
     }
+    // ~Playlist(){
+    //     for(int i=0; i<NumMovies; i++){
+    //         delete movies[i];
+    //     }
+    //     delete[] movies;
+    // }
 };
 
 class User{
     public:
     string name;
-    Playlist* playlist;
+    Playlist** playlists;
     int numPlaylists = 0;
     
-    User(const string& name) : name(name){}
+    User(const string& name) : name(name), playlists(nullptr){
+        // playlists = new Playlist*[200];
+    }
+    
+    ~User(){
+        for(int i=0; i< numPlaylists ; i++){
+            delete playlists[i];
+        }
+        delete[] playlists;
+    }
     
     void createPlaylist(const string& playlistName){
-        playlist[numPlaylists].name = playlistName;
+        Playlist** temp = new Playlist*[numPlaylists + 1];
+        
+        for(int i=0; i< numPlaylists; i++){
+            temp[i] = playlists[i];
+        }
+        temp[numPlaylists] = new Playlist(playlistName);
+        
+        delete[] playlists;
+        playlists = temp;
         numPlaylists++;
+        
     }
     void addMovietoPlaylist(Playlist* playlist, Movie* movie){
-        playlist[numPlaylists].addMovie(movie);
-        numPlaylists++;
+        for(int i=0; i<numPlaylists ; i++){
+            if(playlist->name == playlists[i]->name){
+                playlists[i]->addMovie(movie);
+                break;
+            }
+        }
     }
+    // void addMovietoPlaylist(const string& playlistName, Movie* movie){
+    //     // playlist[numPlaylists].addMovie(movie);
+    //     // playlist[numPlaylists]->addMovie(movie);
+    //     for(int i=0; i<numPlaylists ; i++){
+    //         if(playlistName == playlists[i]->name){
+    //             playlists[i]->addMovie(movie);
+    //             break;
+    //         }
+    //     }
+    // }
     void removeMovieFromPlaylist(Playlist* playlist, Movie* movie){
         playlist[numPlaylists].removeMovie(movie);
-        numPlaylists--;        
+        for(int i=0; i<numPlaylists-1; i++){
+            playlists[i] = playlists[i+1];
+        }
+        numPlaylists--;
     }
     void displayUserPlaylists(){
         cout << "Playlists " << endl;
         for(int i=0; i<numPlaylists; i++){
-            cout << "user playlist = " << playlist[i].name;
+            playlists[i]->displayInfo();
         }
     }
 };
@@ -85,6 +137,8 @@ int main(){
     b("Inception","Christopher Nolan", 60),
     c("The Hangover", "Todd Philliphs", 30),
     d("Superbad", "Greg Mottola", 50);
+    // Movie* a = new Movie("The Dark Knight", "Christopher Nolan", 60);
+    // Movie* b = new Movie("Inception", "Christopher Nolan", 60);
 
     Playlist play1("Action Movies"),
     play2("Comedy Movies");
@@ -93,10 +147,13 @@ int main(){
 
     // play1.addMovie(&a);
     // play1.addMovie(&b);
+    
+    // a1.addMovietoPlaylist("Action Movies", &a);
+    // a1.addMovietoPlaylist("Action Movies", &b);
 
     a1.addMovietoPlaylist(&play1, &a);
     a1.addMovietoPlaylist(&play1, &b);
-
+    
     a1.displayUserPlaylists();
 }
 
