7af91b6dc8509cf97cde3e36786f6515eca7a87a
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-03-28 23:22:11 +0500

    Update task 2.cpp

--- OOPS/lab 9/task 2.cpp
+++ OOPS/lab 9/task 2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Shape{
@@ -24,15 +25,15 @@ class Circle : public Shape {
         cout << "Enter the radius of the Circle" << endl;
         cin >> this->r;
     }
-    virtual double area() const{
+    double area() const{
         return (3.14 * r * r);
     }
 
-    virtual double perimeter() const{
+     double perimeter() const{
         return (2 * 3.14 * r);
     }
 
-    virtual void displayProperties() const{
+     void displayProperties() const{
         cout << "Properties of Circle" << endl;
         cout << "-Area = " << area() << endl
         << "-Perimeter = " << perimeter() << endl
@@ -50,15 +51,15 @@ class Rectangle : public Shape{
         cin >> this->width;
     }
 
-    virtual double area() const{
+     double area() const{
         return (length * width);
     }
 
-    virtual double perimeter() const{
+     double perimeter() const{
         return (length*2) + (width *2);
     }
 
-    virtual void displayProperties() const{
+     void displayProperties() const{
         cout << "Properties of Rectangle" << endl;
         cout << "-Area = " << area() << endl
         << "-Perimeter = " << perimeter() << endl
@@ -74,15 +75,15 @@ class Square : public Shape{
         cout << "Enter the side of the Square" << endl;
         cin >> this->side;
     }
-    virtual double area() const{
+     double area() const{
         return (side * side);
     }
 
-    virtual double perimeter() const{
+     double perimeter() const{
         return (side*4);
     }
 
-    virtual void displayProperties() const{
+     void displayProperties() const{
         cout << "Properties of Square" << endl;
         cout << "-Area = " << area() << endl
         << "-Perimeter = " << perimeter() << endl
@@ -97,16 +98,16 @@ class Triangle : public Shape{
         cout << "Enter the lengths of the three sides of the triangle" << endl;
         cin >> this->s1 >> this->s2 >> this->s3;
     }
-    virtual double area() const{
+     double area() const{
         double s = (s1 + s2 + s3) /3;
         return sqrt(s * (s-s1) * (s-s2)*(s-s3));
     }
 
-    virtual double perimeter() const{
+     double perimeter() const{
         return (s1 + s2 + s3);
     }
 
-    virtual void displayProperties() const{
+     void displayProperties() const{
         cout << "Properties of Triangle" << endl;
         cout << "-Area = " << area() << endl
         << "-Perimeter = " << perimeter() << endl;
@@ -121,29 +122,29 @@ class EquilateralTriangle : public Triangle{
         cin >> this->s;
     }
 
-    virtual double area() const{
+     double area() const{
         return (sqrt(3)/4 * (s*s));
     }
 
-    virtual double perimeter() const{
+     double perimeter() const{
         return (3 * s);
     }
 
-    virtual void displayProperties() const{
+     void displayProperties() const{
         cout << "Properties of equilateral Triangle" << endl;
         cout << "-Area = " << area() << endl;
     }
 };
 
-
-
 int main(){
     cout << "Welcome to the Geometry Competition calculator" << endl;
+    Shape *sptr;
+    
     int n; 
     string cont;
     bool check;
     do{
-        cout << "Please select a shape";
+        cout << "Please select a shape" << endl;
         cout << "1. Circle" << endl
         << "2. Rectangle " << endl
         << "3. Square " << endl
@@ -154,21 +155,30 @@ int main(){
 
         switch(n){
             case 1:
-                Circle c;
-                c.displayProperties();
+            {
+                Circle cir;
+                sptr = &cir;
+                break;
+            }case 2:
+            {    Rectangle rec;
+                sptr = &rec;
+                break;
+            }case 3:
+            {    Square sq;
+                sptr = &sq;
+                break;
+            }case 4:
+            {    Triangle tri;
+                sptr = &tri;
+                break;
+            }
             
-            case 2:
-                Rectangle r();
-
-            case 3:
-                Square sq();
-
-            case 4:
-                Triangle tri();
         }
+        sptr->displayProperties();
 
-        cout << "Do you want to calculate properties for another shape? (yes/no) : " << endl;
+        cout << "\nDo you want to calculate properties for another shape? (yes/no) : ";
         cin >> cont;
+        cout << endl;
         check = (cont == "yes") ? 1 : 0;
     }while(check); 
 }
