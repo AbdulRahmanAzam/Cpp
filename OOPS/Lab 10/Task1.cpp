b6019e661ebb2258d31b0f782441b983ffe1a39f
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2024-05-11 17:48:43 +0500

    Update Task1.cpp

--- OOPS/Lab 10/Task1.cpp
+++ OOPS/Lab 10/Task1.cpp
@@ -1,1 +1,61 @@
+
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Logo{
+    string logo;
+
+    friend class Platform;
+    public:
+    Logo(string logo) : logo(logo) {}
+
+    void setLogo(string logo){
+        this->logo = logo;
+    }
+    void display(){
+        cout << "Original Logo" << endl;
+    }
+};
+class Platform{
+    string name;
+    Logo logo;
+
+    public:
+    Platform(string name, Logo logo) : name(name), logo(logo) {}
+
+    void changeLogo(Logo newlogo){
+        this->logo = newlogo.logo;
+    }
+    void display(){
+        cout << "Platform name: " << name << endl;
+        logo.display();
+    }
+};
+class Server{
+    string location;
+
+public:
+    Server(string location) : location(location) {}
+
+    void display() {
+        cout << "Server location: " << location << endl;
+    }
+};
+
+
+int main(){
+    Logo original_logo("Blue Bird");
+    Platform twitter("Twitter", original_logo);
+
+    original_logo.display();
+    twitter.display();
+
+    original_logo.setLogo("Doge Coin");
+    twitter.changeLogo(original_logo);
+
+    cout << endl;
+    original_logo.display();
+    twitter.display();
+}
